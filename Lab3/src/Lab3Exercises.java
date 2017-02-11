d30c99fbf562d36029f8bc697770fccaca770e53
urielmalave <urielmalave@uriels-mbp.lan>
2017-02-10 21:59:00 -0400

    Change title, size and location of the JFrame.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,11 @@
 import javax.swing.JFrame;
- 
+
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Uriel Malave");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(800, 400);
-		myFrame.setSize(200, 200);
-		myFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Uriel Malave");
+        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        myFrame.setLocation(600, 400);
+        myFrame.setSize(200, 200);
+        myFrame.setVisible(true);
+    }
 }
